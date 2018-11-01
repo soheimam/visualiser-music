943d5536edad5c6eba76481ab5f522bd5dff1a83
Soheima Canton <soheima@Soheimas-MacBook-Pro.local>
2018-11-01 22:45:58 +0100

    fixing directory

--- assets/javascript/music.js
+++ assets/javascript/music.js
@@ -4,22 +4,22 @@ function myFunction() {
 let song;
 let button;
 let fft = new p5.FFT();
-let colorPalette = ["#0C0A3E", "#72fbfd", "#54B1D9", "#00CC99", "#5dff00"];
+let colorPalette = ["#0C0A3E", "#e500ff", "#54B1D9", "#00CC99", "#5dff00"];
 
 
 
   function preload() {
-    song = loadSound('song5.mp3');
+    song = loadSound('../assets/music/song.mp3');
   }
-  
+
   function setup() {
     createCanvas(windowWidth, windowHeight);
     button = createButton('Pause');
     button.mousePressed(toggleSong);
     song.play();
     fft = new p5.FFT();
-    pieces = 8;
-    radius = windowHeight / 4;
+    pieces = 6;
+    radius = windowHeight / 2;
     
   }
 
@@ -30,18 +30,18 @@ let colorPalette = ["#0C0A3E", "#72fbfd", "#54B1D9", "#00CC99", "#5dff00"];
     background(colorPalette[0]);
 
     fft.analyze();
-    let bass = fft.getEnergy(10,250);
-    let treble = fft.getEnergy(100, 150);
-    let mid = fft.getEnergy(60, 250);
+    var bass = fft.getEnergy(10,250);
+    var treble = fft.getEnergy(100, 150);
+    var mid = fft.getEnergy(60, 250);
 
-    let mapbass = map(bass, 0, 255, -100, 800);
-    let scalebass = map(bass, 0, 255, 0.5, 1.2);
+    var mapbass = map(bass, 0, 255, -100, 800);
+    var scalebass = map(bass, 0, 255, 0.5, 1.2);
 
-    let mapTreble = map(treble, 0, 255, -radius / 2, radius * 2);
-    let scaleTreble = map(treble, 0, 255, 1, 1.5);
+    var mapTreble = map(treble, 0, 255, -radius / 2, radius * 2);
+    var scaleTreble = map(treble, 0, 255, 1, 1.5);
 
-    let mapmid = map(mid, 0, 255, -radius * 2, radius * 2);
-    let scalemid = map(mid, 60, 255, 1, 1.5);
+    var mapmid = map(mid, 0, 255, -radius * 2, radius * 2);
+    var scalemid = map(mid, 60, 255, 1, 1.5);
 
     mapMouseX = map(mouseX, 0, width, 2, 0.1);
     mapMouseY = map(mouseY, 0, height, windowHeight / 4, windowHeight / 6);
@@ -55,22 +55,22 @@ let colorPalette = ["#0C0A3E", "#72fbfd", "#54B1D9", "#00CC99", "#5dff00"];
 
     for (i = 0; i < pieces; i += 0.01) {
 
-        translate( width/2, height/2 );
-        rotate(QUARTER_PI * pieces);
+        rotate(TWO_PI * pieces);
        
 
         push();
         strokeWeight(1);
-        stroke(colorPalette[1]);
+        stroke(colorPalette[2]);
         scale(scalebass);
         rotate(frameCount * -0.5);
         line(mapbass, radius / 2, radius, radius);
+  
         line(-mapbass, -radius / 2, radius, radius);
         pop();
 
      
         push();
-        stroke(colorPalette[3]);
+        stroke(colorPalette[1]);
         scale(scaleTreble * 0.5);
         line(mapTreble, radius / 2, radius, radius);
         pop();
